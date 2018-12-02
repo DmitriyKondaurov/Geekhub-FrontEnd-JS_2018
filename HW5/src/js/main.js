f5d07f67848787ab1d262fe413c616dc1019fc28
Dmytro Kondaurov <invisibl@ukr.net>
2018-12-02 22:03:59 +0200

    add top nav responsive sandwich button

--- HW5/src/js/main.js
+++ HW5/src/js/main.js
@@ -8,3 +8,24 @@ $(document).ready(function() {
         }
     });
 });
+
+// Toggle sandwich button
+$("#sandwich, .top-nav_item").click(function() {
+    $("#sandwich").toggleClass("active");
+});
+
+//fade out/in menu list
+$(".toggle-mnu-btn").click(function () {
+    if($("#sandwich").hasClass("active")) {
+        $(".top-mnu").fadeIn(600).css("display", "inline-block");
+    }
+    else {
+        $(".top-mnu").fadeOut(600);
+    }
+});
+//close menu on click
+$(".top-mnu li a").click(function () {
+    if($(".toggle-mnu-btn").is(':visible')) {
+        $(".top-mnu").fadeOut(600);
+    }
+});
