4100cb2a967bba2bb97a2cd69a724836ddc0f56f
Dmytro Kondaurov <invisibl@ukr.net>
2018-12-06 16:04:07 +0200

    first test done

---
+++ HW_Js#1/test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+test('test_string', () => {
+    expect('strict').toEqual('strict');
+});
+
+test('test_numbers', () => {
+    expect().toEqual();
+});
+
+test('test_arrays', () => {
+    expect().toEqual();
+});
+
+test('test_compare_operators', () => {
+    expect().toEqual();
+});
+
+test('test_equality_comparison_algorithm', () => {
+    expect().toEqual();
+});
