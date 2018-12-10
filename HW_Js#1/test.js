1689099ea9590ed5434226787174a6256f03d4b9
Dmytro Kondaurov <invisibl@ukr.net>
2018-12-10 09:06:45 +0200

    numbers done

--- HW_Js#1/test.js
+++ HW_Js#1/test.js
@@ -4,8 +4,28 @@ test('test_string', () => {
     expect('strict').toEqual('strict');
 });
 
-test('test_numbers', () => {
-    expect().toEqual();
+test('test_numbers_parseFloat', () => {
+    expect(Number.parseFloat('5.0')).toEqual(5);
+});
+
+test('test_numbers_parseInt', () => {
+    expect(Number.parseInt('5.5')).toEqual(5);
+});
+
+test('test_numbers_isFinite', () => {
+    expect(Number.isFinite(5.5)).toEqual(true);
+});
+
+test('test_numbers_isInteger', () => {
+    expect(Number.isInteger(5.5)).toEqual(false);
+});
+
+test('test_numbers_isNaN', () => {
+    expect(Number.isNaN('5.5')).toEqual(false);
+});
+
+test('test_numbers_isSafeInteger', () => {
+    expect(Number.isSafeInteger(5)).toEqual(true);
 });
 
 test('test_arrays', () => {
